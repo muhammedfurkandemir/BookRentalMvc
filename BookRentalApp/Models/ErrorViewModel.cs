3a519f60ae0ab6302daf8bfcbd40fae8375f7a57
Furkan Demir <furkan.vty0@gmail.com>
2023-11-11 14:21:14 +0300

    Proje dosyası ekle.

---
+++ BookRentalApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookRentalApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
