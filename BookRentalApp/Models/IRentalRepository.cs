0b987a8d863274a3a85d71089218f0f38dd48e43
Furkan Demir <furkan.vty0@gmail.com>
2023-12-02 13:17:01 +0300

    Add RentalRepository

---
+++ BookRentalApp/Models/IRentalRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace BookRentalApp.Models
+{
+    public interface IRentalRepository:IRepository<Rental>
+    {
+        void Update(Rental rental);
+
+        void Save();
+    }
+}
---
+++ BookRentalApp/Models/RentalRepository.cs
@@ -0,0 +1,24 @@
+﻿using BookRentalApp.Utility;
+using System.Linq.Expressions;
+
+namespace BookRentalApp.Models
+{
+    public class RentalRepository : Repository<Rental>, IRentalRepository
+    {
+        private RentalBookContext _rentalBookContext;
+        public RentalRepository(RentalBookContext rentalBookContext) : base(rentalBookContext)
+        {
+            _rentalBookContext = rentalBookContext;
+        }
+
+        public void Save()
+        {
+            _rentalBookContext.SaveChanges();
+        }
+
+        public void Update(Rental rental)
+        {
+            _rentalBookContext.Update(rental);
+        }
+    }
+}
